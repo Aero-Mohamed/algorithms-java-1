2b01e8c917b35cc2c800c4b22c2e9ee7f0b8b3aa
Mohamed Ahmed <aero.mohamed@icloud.com>
2025-12-26 15:43:14 +0200

    Java Vector and ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
+        Main.chapterArrays();
+    }
 
+    public static void chapterArrays()
+    {
+        // Vector: Grow 100% of its size - synchronized
+        // ArrayList: Grow 50%
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+        list.remove(Integer.valueOf(1));
+        list.remove(1);
+        System.out.println(list);
+        System.out.println("\nIndexOf 2: " + list.indexOf(2));
+        System.out.println("\nLast Index of 2: " + list.lastIndexOf(2));
+        System.out.println("\nSize: " + list.size());
+        System.out.println("\nlist contains 2: " + list.contains(2));
+
+        System.out.println(
+            "\nRegular Arr: \n" +
+            Arrays.toString(list.toArray(new Integer[0]))
+        );
     }
 
 }
